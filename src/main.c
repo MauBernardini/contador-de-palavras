e4d93488db8e091c1ed7ce3270b7eea92138f0a0
Mauricio Bernardini <mausissio@gmail.com>
2017-08-08 12:24:49 -0300

    nao sei pq esta dando errado

--- src/main.c
+++ src/main.c
@@ -1,22 +1,55 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
 
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+char string[200];
+int caracteres[200];
+void qual_caractere (char c, int i);
 
-  char c;
+int main(){
 
-  c = 1;
+ int cont, i;
+ char c;
+ cont = 0;
+ i=0;
 
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
-
-  printf("1\n");
-  return 0;
+ while(c!='\n'){
+	scanf("%c",&c);
+	string[i]= c;
+	qual_caractere(c,i);
+	i++;}
+ for(i=0;i++;i<199){
+	if(caracteres[i]==0){
+		if(caracteres[i+1]!=0){
+			cont++;}
+			    }
+	if(caracteres[i]==1){
+		if(caracteres[i+1]==0 || caracteres[i+1]==1){
+			cont++;}
+		if(caracteres[i+1]==2){
+			if(string[i+1]!='.'){
+				cont++;}
+				      }
+			    }
+		   }
+ printf("%s\n",string);
+ for(i=0;i++;i<200){
+ printf("%d\n",caracteres[i]);}
+ printf("%d\n",cont);
+ return 0;
 }
+
+void qual_caractere (char c, int i){
+  if(c == '-' || c=='.' || c==',' || c==':' || c==';'){
+	caracteres[i]=2;}
+  if(c==' '||c=='\n'){
+	caracteres[i]=1;}
+  if(c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9'){
+	caracteres[i]=3;}
+  else{
+	caracteres[i]=0;}
+ return;
+}
+				
+
+
